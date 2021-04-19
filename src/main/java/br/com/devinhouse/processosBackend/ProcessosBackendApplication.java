072194e568a9032b1ad5c7b2af27b9424e5878f6
jorj <jorj@whirlpool>
2021-04-19 20:25:56 -0300

    chore: project initialization!

---
+++ src/main/java/br/com/devinhouse/processosBackend/ProcessosBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.devinhouse.processosBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessosBackendApplication.class, args);
+	}
+
+}
